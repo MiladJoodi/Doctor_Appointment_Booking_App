f80e96cf2fcfcc05ab447a611e20abc58921e4c6
Milad Joodi <winbeta.com@gmail.com>
2024-03-02 19:11:44 +0330

    0

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 const Menu = [
@@ -20,20 +21,21 @@ const Menu = [
 
 const Header = () => {
     return (
-        <div>
-            <div>
+        <div className="flex items-center justify-between p-4 shadow-sm">
+            <div className="flex items-center gap-10">
                 <Image
                     src="/logo.svg"
                     alt="logo"
                     width={180}
                     height={80}
                 />
-                <ul className="">
+                <ul className="md:flex gap-8 hidden">
                     {Menu.map((item, index)=>(
                         <li key={index}>{item.name}</li>
                     ))}
                 </ul>
             </div>
+            <Button>Get Started</Button>
         </div>
     );
 }
