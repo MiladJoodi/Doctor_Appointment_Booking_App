e3e031286c47ad85155fe80f09c9d5c36a03cd3f
Milad Joodi <winbeta.com@gmail.com>
2024-03-02 23:15:44 +0330

    Strapi

---
+++ app/_components/CategorySearch.jsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const CategorySearch = () => {
+  return (
+    <div className="mb-10 flex flex-col items-center gap-2">
+
+      <h2 className="font-bold text-4xl tracking-wide">
+        Search <span className="text-primary">Doctors</span>
+      </h2>
+
+      <h2 className="text-gray-400 text-xl">
+        Search Your Doctor and Book Appointment in one click
+      </h2>
+
+      <div className="flex w-full max-w-sm mt-3 items-center space-x-2">
+        <Input type="email" placeholder="Search..." />
+        <Button type="submit">
+            <Search className="h-4 w-4 mr-2" />
+            Search
+            </Button>
+      </div>
+    </div>
+  );
+};
+
+export default CategorySearch;
