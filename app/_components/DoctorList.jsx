ca6acd0e9f0a8846f5c08ae6766c101351f16b0c
Milad Joodi <winbeta.com@gmail.com>
2024-03-04 02:18:26 +0330

    5

---
+++ app/_components/DoctorList.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const DoctorList = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default DoctorList;
--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Hero from "./_components/Hero";
 import CategorySearch from "./_components/CategorySearch";
+import DoctorList from "./_components/DoctorList";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <CategorySearch />
+      <DoctorList />
     </main>
   );
 }
